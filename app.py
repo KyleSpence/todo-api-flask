973ba76bfc61ef265454e6a0f662dda56e5712b5
Rameez Khan <rameezkhan.sa@gmail.com>
2019-03-13 21:27:19 +0200

    Implement mongodb crud operations

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
 from flask import Flask, jsonify, abort, make_response, request
 from flask_pymongo import PyMongo
 from pymongo.errors import ConnectionFailure
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/admin"
 mongo = PyMongo(app)
 
-tasks = [
-  {
-    'id': 1,
-    'title': u'Buy groceries',
-    'description': u'Milk, Cheese, Cucumber',
-    'done': False
-  },
-  {
-    'id': 2,
-    'title': u'Learn Flask',
-    'description': u'Need to learn Flask',
-    'done': False
-  }
-]
+
+def get_tasks_from_db():
+  return list(mongo.db.tasks.find({}, {'_id': 0}))
+
+
+def get_task_from_db(task_id):
+  return list(mongo.db.tasks.find({"id": task_id}, {"_id": 0}))
+
+
+def delete_task_from_db(task_id):
+  mongo.db.tasks.delete_one({"id": task_id})
+
+
+def update_task_from_db(task_id, task):
+  mongo.db.tasks.update_one({"id": task_id}, {"$set": task}, upsert=False)
 
 
 @app.errorhandler(404)
 def not_found_error(error):
   return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.route('/health')
 def get_health():
@@ -35,14 +38,16 @@ def get_health():
   except ConnectionFailure:
     abort(500)
 
+
 @app.route('/todo/api/tasks', methods=['GET'])
 def get_tasks():
+  tasks = get_tasks_from_db()
   return jsonify({'tasks': tasks})
 
 
 @app.route('/todo/api/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
+  task = get_task_from_db(task_id)
   if len(task) == 0:
     abort(404)
   return jsonify({'task': task[0]})
@@ -52,19 +57,29 @@ def get_task(task_id):
 def create_task():
   if not request.json or not 'title' in request.json:
     abort(400)
+  tasks = get_tasks_from_db()
+
+  task_id = 1
+  if len(tasks) > 0:
+    task_id = tasks[-1]['id'] + 1
+
   task = {
-    'id': tasks[-1]['id'] + 1,
+    'id': task_id,
     'title': request.json['title'],
     'description': request.json.get('description', ""),
     'done': False
   }
-  tasks.append(task)
-  return jsonify({'task': task}), 201
+  task_object_id = mongo.db.tasks.insert_one(task).inserted_id
+
+  if task_object_id:
+    return jsonify({'success': True}), 201
+  else:
+    abort(400)
 
 
 @app.route('/todo/api/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
+  task = get_task_from_db(task_id)
   if len(task) == 0:
     abort(404)
   if not request.json:
@@ -78,15 +93,16 @@ def update_task(task_id):
   task[0]['title'] = request.json.get('title', task[0]['title'])
   task[0]['description'] = request.json.get('description', task[0]['description'])
   task[0]['done'] = request.json.get('done', task[0]['done'])
-  return jsonify({'task': task[0]})
+  update_task_from_db(task_id, task[0])
+  return jsonify({'success': True})
 
 
 @app.route('/todo/api/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
-  task = [task for task in tasks if task['id'] == task_id]
+  task = get_task_from_db(task_id)
   if len(task) == 0:
     abort(404)
-  tasks.remove(task[0])
+  delete_task_from_db(task_id)
   return jsonify({'result': True})
 
 
